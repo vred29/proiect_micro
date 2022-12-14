85cc4583eb427b15c65b7803d1382ee83c4667d4
raul.vega <raulvegakinder@gmail.com>
2022-12-14 16:12:08 +0200

    Creare grafice pyqt
    Signed-off-by: raul.vega <raulvegakinder@gmail.com>

--- pyqt/ui_micro.py
+++ pyqt/ui_micro.py
@@ -8,142 +8,118 @@ from random import randint
 import serial.tools.list_ports
 from PyQt5.QtWidgets import *
 from PyQt5 import QtGui,QtCore
-
 import re
 
-input_buff= [0] * 5
-serialInstance=serial.Serial()
-port="COM10"
-serialInstance.baudrate=115200
-serialInstance.port=port
-serialInstance.open()
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
+portsList = []
+
+for onePort in ports:
+    portsList.append(str(onePort))
+    print(str(onePort))
+
+val = input("Select Port: COM")
+
+for x in range(0,len(portsList)):
+    if portsList[x].startswith("COM" + str(val)):
+        portVar = "COM" + str(val)
+        print(portVar)
+
+serialInst.baudrate = 115200
+serialInst.port = portVar
+serialInst.open()
 
 class MainWindow(QtWidgets.QMainWindow):
 
-    decibels=0
-    temperature=0
+    rotation_sensor=0
+    light_sensor=0
+
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         
         self.widget = QWidget()
         layout = QGridLayout()
         self.widget.setLayout(layout)
+
         
-        self.graphWidget_sound = pg.PlotWidget()
-        self.graphWidget_flame = pg.PlotWidget()
+        self.graphWidget_light = pg.PlotWidget()
+        self.graphWidget_rotation = pg.PlotWidget()
         
-        layout.addWidget(self.graphWidget_sound, 0, 0)
-        layout.addWidget(self.graphWidget_flame, 0, 1)
+        layout.addWidget(self.graphWidget_light, 0, 0)
+        layout.addWidget(self.graphWidget_rotation, 0, 1)
 
         self.setCentralWidget(self.widget)
-        #self.setCentralWidget(self.graphWidget)
-        self.setWindowTitle("Light sensor")
+        self.setWindowTitle("Setting threshold to light sensor")
 
-        #icon
-        scriptDir = os.path.dirname(os.path.realpath(__file__))
-        self.setWindowIcon(QtGui.QIcon(scriptDir + os.path.sep + 'sound.png'))
-
-        self.xs = list(range(100))  # 100 time points
-        self.ys = [30 for _ in range(100)]  # 100 data points
+        self.xr = list(range(100))  # 100 time points
+        self.yr = [30 for _ in range(100)]  # 100 data points
         
-        self.xf = list(range(100))  # 100 time points
-        self.yf = [30 for _ in range(100)]  # 100 data points
+        self.xl = list(range(100))  # 100 time points
+        self.yl = [30 for _ in range(100)]  # 100 data points
 
-        self.graphWidget_sound.setBackground('k')
-        self.graphWidget_flame.setBackground('k')
+        self.graphWidget_rotation.setBackground('k')
+        self.graphWidget_light.setBackground('k')
 
-        pen = pg.mkPen(color=(19, 252, 3))
-        self.data_line =  self.graphWidget_sound.plot(self.xs, self.ys, pen=pen)
+        color1 = pg.mkPen(color=(0, 255, 255))
+        self.data_line =  self.graphWidget_rotation.plot(self.xr, self.yr, pen=color1)
 
-        pen2 = pg.mkPen(color=	(255,0,0))
-        self.data_line2 =  self.graphWidget_flame.plot(self.xf, self.yf, pen=pen2)
+        color2 = pg.mkPen(color=(255,0,0))
+        self.data_line2 =  self.graphWidget_light.plot(self.xl, self.yl, pen=color2)
     
-    def update_plot_data_sound(self):
-        self.xs = self.xs[1:]  # Remove the first y element.
-        self.xs.append(self.xs[-1] + 1)  # Add a new value 1 higher than the last.
-
-        self.ys = self.ys[1:]  # Remove the first 
-        self.ys.append(self.decibels)  
-        #print(self.decibels)
-        self.data_line.setData(self.xs, self.ys)  # Update the data.
-
-    def update_plot_data_flame(self):
-        self.xf = self.xf[1:]  # Remove the first y element.
-        self.xf.append(self.xf[-1] + 1)  # Add a new value 1 higher than the last.
-
-        self.yf = self.yf[1:]  # Remove the first 
-        self.yf.append(self.temperature)  
-        #print(self.decibels)
-        self.data_line2.setData(self.xf, self.yf)  # Update the data.
-
-    def switch_rgb_sensor(self):
-        global serialInstance
-        #print("switch rgb")
-        serialInstance.write(b'a')
+    def update_plot_data_rotation(self):
+        self.xr = self.xr[1:]  # Remove the first y element.
+        self.xr.append(self.xr[-1] + 1)  # Add a new value 1 higher than the last.
+        if self.rotation_sensor<6000:
+            self.yr = self.yr[1:]  # Remove the first 
+            self.yr.append(self.rotation_sensor)  
+            self.data_line.setData(self.xr, self.yr)  # Update the data.
         
-read_data=1
-i=0
+    def update_plot_data_light(self):
+        self.xl = self.xl[1:]  # Remove the first y element.
+        self.xl.append(self.xl[-1] + 1)  # Add a new value 1 higher than the last.
 
-def Average(lst):
-    return sum(lst) / len(lst)
+        self.yl = self.yl[1:]  # Remove the first 
+        self.yl.append(self.light_sensor)  
+        if self.light_sensor>0 and self.light_sensor<2000:
+            # daca e valoare mica => VERDE (0,255,0)
+            color1 = pg.mkPen(color=(0,255,0))    
+            self.data_line2.setData(self.xl, self.yl, pen = color1)  # Update the data.
+        elif self.light_sensor>2000 and self.light_sensor<4000:
+            # daca e valoare mica => GALBEN (255,255,0)
+            color2 = pg.mkPen(color=(255,255,0))    
+            self.data_line2.setData(self.xl, self.yl, pen=color2)
+        elif self.light_sensor>4000 and self.light_sensor<6000:
+            # daca e valoare mica => RED (255,0,0)
+            color3 = pg.mkPen(color=(255,0,0))    
+            self.data_line2.setData(self.xl, self.yl, pen=color3)
+            # Update the data.
 
 def get_data_from_serial():
-    global read_data
-    global serialInstance
-    global window
+    global window1
     global data
-    global i
-    if serialInstance.in_waiting:
-        packet=serialInstance.readline()
+    if serialInst.in_waiting:
+        packet=serialInst.readline()
        
         buffer=packet.decode('utf',errors='replace').strip("\n")
-       
-        
-        #
-        if buffer[1]=='S':
-            
-            
-            #print(buffer)
-            buffer=buffer[2:]
-            buff=([int(s) for s in buffer.split() if s.isdigit()][0])
-            data=buff
-            
-            #input_buff.append(data)
-            #input_buff.pop(0)
-            #i=i+1
-            
-            #print(input_buff)
-            #average = Average(input_buff)
-            #if (input_buff[3]-data)<50 :
-            window.decibels=data
-          
-                
-            #if i==10000:
-            #    i=0
-            
-            
-            
-            
-        else :
-            #print(buffer)
-            buffer=buffer[2:]
-            buff=([int(s) for s in buffer.split() if s.isdigit()][0])
-            data=buff
-
-            window.temperature=data
-            
-            
-            
-        
-        
-
-data=0
-
+        aux=buffer.split(" ")
+        if len(aux) == 3:
+            if aux[1].isnumeric():
+                if "Light" in aux[0]:
+                    data=aux[1]
+                    print(data)
+                    window1.light_sensor=int(data)
+                    
+                else :
+                    aux=buffer.split(" ")
+                    data=aux[1]
+                    print(data)
+                    window1.rotation_sensor=int(data)
 
 
 app = QtWidgets.QApplication(sys.argv)
-window = MainWindow()
-window.show()
+window1 = MainWindow()
+window1.show()
+
 
 timer = QtCore.QTimer()
 timer2 = QtCore.QTimer()
@@ -151,13 +127,11 @@ timer2 = QtCore.QTimer()
 timer.setInterval(50)
 timer2.setInterval(1)
 timer2.timeout.connect(get_data_from_serial)
-#window.decibels=data
-timer.timeout.connect(window.update_plot_data_sound)
-timer.timeout.connect(window.update_plot_data_flame)
+
+timer.timeout.connect(window1.update_plot_data_rotation)
+timer.timeout.connect(window1.update_plot_data_light)
 timer2.start()
 timer.start()
 
-
-    
 sys.exit(app.exec_())
             
